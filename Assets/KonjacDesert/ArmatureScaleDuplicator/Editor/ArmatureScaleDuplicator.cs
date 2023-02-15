b982bd636421bb2386db9af0c5cb7db06cdda753
KonjacDesert <56007373+konjacdesert@users.noreply.github.com>
2023-02-15 15:52:24 +0900

    Change License

--- Assets/KonjacDesert/ArmatureScaleDuplicator/Editor/ArmatureScaleDuplicator.cs
+++ Assets/KonjacDesert/ArmatureScaleDuplicator/Editor/ArmatureScaleDuplicator.cs
@@ -1,20 +1,16 @@
-﻿using System.Collections;
+﻿/*
+
+Copyright © 2022 KonjacDesert
+
+This work is free. You can redistribute it and/or modify it under the terms of the Do What The Fuck You Want To Public License, Version 2, as published by Sam Hocevar. See the COPYING file for more details.
+
+*/
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using System.Linq;
-
-/*
- * 
- * ArmatureScaleDuplicator
- * 
- * rev 1
- * 
- * date 20230215
- * 
- * author KonjacDesert
- * 
- */
 
 namespace KonjacDesert
 {
